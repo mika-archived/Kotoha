5b247b078490f9ef6c9351f2d940127e6f55533b
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-06-29 20:55:26 +0900

    chore(debug): Allow internal access for LINQPad.

--- Source/Kotoha/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Kotoha
-{
-    public class Class1
-    {
-    }
-}
---
+++ Source/Kotoha/KotohaPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Kotoha
+{
+    public class KotohaPlayer
+    {
+        public void LoadPlugins(string directory, bool recursive = false)
+        {
+            //
+        }
+
+        public async Task PlayAsync(string text, string talker) { }
+
+        public async Task SaveAsync(string text, string talker, string path) { }
+    }
+}
